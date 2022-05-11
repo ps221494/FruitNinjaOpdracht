a0d92ec57b617b8b4ab91e217496d762f2b58628
Dotwood Media <info@dotwood.media>
2022-05-11 09:16:50 +0200

    Tailwind config

--- fruit-ninja/App.js
+++ fruit-ninja/App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw.style('bg-blue-100')}>
+      <Text style={tw.style('text-md', invalid && 'text-red-500')}>Hello</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

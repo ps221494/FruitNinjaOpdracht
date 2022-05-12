fcd00dfacdbd06fbdcd9f366bac4a781cdd267cd
Dotwood Media <info@dotwood.media>
2022-05-12 12:08:07 +0200

    Update App.js

--- fruit-ninja/App.js
+++ fruit-ninja/App.js
@@ -1,96 +1,84 @@
 import React, { useRef, useEffect } from 'react';
-import { Text, SafeAreaView, View, ImageBackground, Animated, Dimensions, Easing } from 'react-native';
+import { Text, SafeAreaView, View, ImageBackground, Animated, Dimensions, Easing, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
-  const moveValue = useRef(new Animated.ValueXY()).current;
+  const moveValue = useRef(new Animated.ValueXY({
+    x: 0,
+    y: Dimensions.get('window').height + 70
+  })).current;
   const spinValue = new Animated.Value(0);
 
-  const [width, setWidth] = React.useState(0);
-  const [height, setHeight] = React.useState(0);
+  const [height, setHeight] = React.useState(Dimensions.get('window').height * 0.25);
+  const [image, setImage] = React.useState('https://i.pinimg.com/originals/4a/bb/5a/4abb5afb6d042b709dfb53ea108d52a2.png');
 
-  function getScreenValue() {
-    const fullWidth = Dimensions.get('window').width - 70;
-    const fullHeight = Dimensions.get('window').height - 70;
-
-    setWidth(fullWidth / 2);
-    setHeight(fullHeight / 2);
-
-    console.log(fullWidth / 2);
-  }
-
-  function generateX() {
-    let number = Math.floor(Math.random() * (Dimensions.get('window').width - 70));
-    if (moveValue.x === number) number = Math.floor(Math.random() * (Dimensions.get('window').width - 70));
-    return number;
-  }
-
-  function generateY() {
-    let number = Math.floor(Math.random() * (Dimensions.get('window').height - 70));
-    if (moveValue.y === number) number = Math.floor(Math.random() * (Dimensions.get('window').height - 70));
-    return number;
-  }
+  let images = ['https://i.pinimg.com/originals/4a/bb/5a/4abb5afb6d042b709dfb53ea108d52a2.png', 'https://freepikpsd.com/file/2019/10/banana-cartoon-png-Images-PNG-Transparent.png']
 
   const spin = spinValue.interpolate({
     inputRange: [0, 1],
     outputRange: ['0deg', '360deg']
   })
 
+  function getRandomWidth() {
+    return Math.floor(Math.random() * (Dimensions.get('window').width - 70));
+  }
+
   useEffect(() => {
-    getScreenValue();
-    // Animated.timing(
-    //   spinValue,
-    //   {
-    //     toValue: 1,
-    //     duration: 3000,
-    //     easing: Easing.linear,
-    //   }
-    // ).start()
+    function cycleAnimation() {
+      let width = getRandomWidth();
+      console.log(width)
+      Animated.sequence([
+        Animated.timing(moveValue, {
+          toValue: { x: getRandomWidth(), y: Dimensions.get('window').height + 70 },
+          duration: 1000,
+          ease: Easing.ease,
+          delay: 0,
+          useNativeDriver: false
+        }),
+        Animated.timing(moveValue, {
+          toValue: { x: getRandomWidth(), y: height },
+          duration: 1000,
+          ease: Easing.ease,
+          delay: 0,
+          useNativeDriver: false
+        })
+      ]).start(() => {
+        cycleAnimation();
+      });
+    }
 
-    // function cycleAnimation() {
-    //   Animated.sequence([
-    //     Animated.timing(moveValue, {
-    //       toValue: { x: generateX(), y: generateY() },
-    //       duration: 500,
-    //       ease: Easing.ease,
-    //       delay: 0
-    //     }),
-    //     Animated.timing(moveValue, {
-    //       toValue: { x: generateX(), y: generateY() },
-    //       duration: 500,
-    //       ease: Easing.ease,
-    //       delay: 0
-    //     })
-    //   ]).start(() => {
-    //     cycleAnimation();
-    //   });
-    // }
+    function cycleRotation() {
+      Animated.sequence([
+        Animated.timing(
+          spinValue,
+          {
+            toValue: 1,
+            duration: 3000,
+            easing: Easing.linear,
+            useNativeDriver: false
+          }
+        ),
+        Animated.timing(
+          spinValue,
+          {
+            toValue: 0,
+            duration: 1,
+            easing: Easing.linear,
+            useNativeDriver: false
+          }
+        )
+      ]).start(() => {
+        cycleRotation();
+      });
+    }
 
-    // function cycleRotation() {
-    //   Animated.sequence([
-    //     Animated.timing(
-    //       spinValue,
-    //       {
-    //         toValue: 1,
-    //         duration: 3000,
-    //         easing: Easing.linear,
-    //       }
-    //     ),
-    //     Animated.timing(
-    //       spinValue,
-    //       {
-    //         toValue: 0,
-    //         duration: 1,
-    //         easing: Easing.linear,
-    //       }
-    //     )
-    //   ]).start(() => {
-    //     cycleRotation();
-    //   });
-    // }
+    function generateImage() {
+      let randomNumber = Math.floor(Math.random() * images.length);
+      setImage(images[randomNumber]);
+    }
 
-    // cycleRotation();
-    // cycleAnimation();
+    cycleRotation();
+    cycleAnimation();
   }, []);
 
   return (
@@ -101,14 +89,17 @@ export default function App() {
           style={[
             {
               transform: [{ rotate: spin }],
-              backgroundColor: "#61dafb",
-              width: 80,
-              height: 80,
-              borderRadius: 4,
+              marginLeft: 10,
+              width: 100,
+              height: 100,
             },
             moveValue.getLayout(),
           ]}
-        />
+        >
+          <Image source={{
+            uri: image,
+          }} style={tw.style('w-full h-full')} />
+        </Animated.View>
       </ImageBackground>
     </SafeAreaView>
   );

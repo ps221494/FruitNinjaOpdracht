f9f5dc725adc64ea3201e22ddef0d4cf2d8cdad8
Dotwood Media <info@dotwood.media>
2022-06-01 09:13:54 +0200

    Code verbeterd
    Animation klopt helemaal en alle code even goed gezet

--- fruit-ninja/App.js
+++ fruit-ninja/App.js
@@ -1,109 +1,137 @@
 import React, { useRef, useEffect } from 'react';
-import { Text, SafeAreaView, View, ImageBackground, Animated, Dimensions, Easing, Image, TouchableOpacity } from 'react-native';
+import { Text, SafeAreaView, ImageBackground, Animated, Dimensions, Easing, Image, TouchableOpacity } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
-import BgAudio from 'react-native-background-audio'
 
 export default function App() {
+  // Use state to store some values
+  const [points, setPoints] = React.useState(0);
+  const [isPlaying, setIsPlaying] = React.useState(true);
+  const [height, setHeight] = React.useState(Dimensions.get('window').height * 0.25);
+
+  // The values of the animations
   const moveValue = useRef(new Animated.ValueXY({
     x: 0,
     y: Dimensions.get('window').height + 70
   })).current;
   const spinValue = new Animated.Value(0);
-  const [points, setPoints] = React.useState(0);
 
-  const [height, setHeight] = React.useState(Dimensions.get('window').height * 0.25);
-
-  //images that are used in the game
-  const [image, setImage] = React.useState(require('./assets/1.png'),
-    require('./assets/2.png'),
-    require('./assets/3.png'),
-    require('./assets/4.png'),
-    require('./assets/5.png'),
-    require('./assets/6.png'),);
-
-  let images = ['https://i.pinimg.com/originals/4a/bb/5a/4abb5afb6d042b709dfb53ea108d52a2.png', 'https://freepikpsd.com/file/2019/10/banana-cartoon-png-Images-PNG-Transparent.png']
-
+  // Config for the spin animation
   const spin = spinValue.interpolate({
     inputRange: [0, 1],
     outputRange: ['0deg', '360deg']
   })
 
+  // Images that are used in the game
+  const [image, setImage] = React.useState(
+    require('./assets/1.png'),
+    require('./assets/2.png'),
+    require('./assets/3.png'),
+    require('./assets/4.png'),
+    require('./assets/5.png'),
+    require('./assets/6.png'),
+  );
+
+  // Generate a random width
   function getRandomWidth() {
     return Math.floor(Math.random() * (Dimensions.get('window').width - 70));
   }
 
-  const [audio_options, SetAudio] = React.useState(
-    require('./assets/slashSFX.mp3')
-  );
-
-  // function that changes the image to a random image onclick
+  // Function that changes the image to a random image onclick
   generateImage = () => {
     const randomImage = Math.floor(Math.random() * 6) + 1;
-    let Numb = randomImage;
     setImage(randomImage);
   }
 
-  useEffect(() => {
-    function cycleAnimation() {
-      Animated.sequence([
-        Animated.timing(moveValue, {
-          toValue: { x: getRandomWidth(), y: Dimensions.get('window').height + 70 },
-          duration: 1000,
-          ease: Easing.ease,
-          delay: 0,
-          useNativeDriver: false
-        }),
-        Animated.timing(moveValue, {
-          toValue: { x: getRandomWidth(), y: height },
-          duration: 1000,
-          ease: Easing.ease,
-          delay: 0,
-          useNativeDriver: false
-        })
-      ]).start(() => {
+  // Function that run the animation when the game starts
+  function cycleAnimation() {
+    // Loop the animation
+    Animated.sequence([
+      Animated.timing(moveValue, {
+        toValue: { x: getRandomWidth(), y: Dimensions.get('window').height + 70 },
+        duration: 1000,
+        ease: Easing.ease,
+        delay: 0,
+        useNativeDriver: false
+      }),
+      Animated.timing(moveValue, {
+        toValue: { x: getRandomWidth(), y: height },
+        duration: 1000,
+        ease: Easing.ease,
+        delay: 0,
+        useNativeDriver: false
+      })
+    ]).start(({ finished }) => {
+      // Check if the animation is finished and the game is still running
+      if (finished && isPlaying) {
+        // Restart the animation
         cycleAnimation();
         setTimeout(() => {
+          // Generate a new image
           generateImage();
         }, 1000);
-      });
-    }
+      }
+      else {
+        // Reset the values
+        moveValue.setValue({ x: 0, y: Dimensions.get('window').height + 70 });
+      }
+    });
+  }
 
-    function cycleRotation() {
-      Animated.sequence([
-        Animated.timing(
-          spinValue,
-          {
-            toValue: 1,
-            duration: 3000,
-            easing: Easing.linear,
-            useNativeDriver: false
-          }
-        ),
-        Animated.timing(
-          spinValue,
-          {
-            toValue: 0,
-            duration: 1,
-            easing: Easing.linear,
-            useNativeDriver: false
-          }
-        )
-      ]).start(() => {
+  // Fruit rotate function
+  function cycleRotation() {
+    // Loop the rotation
+    Animated.sequence([
+      Animated.timing(
+        spinValue,
+        {
+          toValue: 1,
+          duration: 3000,
+          easing: Easing.linear,
+          useNativeDriver: false
+        }
+      ),
+      Animated.timing(
+        spinValue,
+        {
+          toValue: 0,
+          duration: 1,
+          easing: Easing.linear,
+          useNativeDriver: false
+        }
+      )
+    ]).start(({ finished }) => {
+      if (finished) {
+        // Restart the rotation
         cycleRotation();
-      });
-    }
+      }
+    });
+  }
 
-    ClickedObject = () => {
-      // adds 1 to the score
-      setTimeout(() => {
-        generateImage();
-      }, 1000);
-    }
+  ClickedObject = () => {
+    // Stop the game
+    setIsPlaying(false);
 
+    // Wait 500ms to restart the game
+    setTimeout(() => {
+      // Generate a new image
+      generateImage();
+      // Start the game again
+      setIsPlaying(true);
+    }, 500);
+  }
+
+  useEffect(() => {
+    if (isPlaying) {
+      // Restart the animation
+      cycleAnimation();
+    }
+    else {
+      // Reset the values and stop the animation
+      moveValue.stopAnimation();
+      moveValue.setValue({ x: 0, y: Dimensions.get('window').height + 70 });
+    }
     cycleRotation();
-    cycleAnimation();
-
-  }, []);
+  }, [isPlaying, cycleRotation, cycleAnimation]);
 
   return (
     <SafeAreaView style={tw.style('flex-1 bg-black')}>
@@ -122,10 +150,9 @@ export default function App() {
         >
           <TouchableOpacity onPress={() => {
             ClickedObject()
+            // Adds 1 to the score
             setPoints(points + 1)
-          }
-          }>
-
+          }}>
 
             <Image source={image} style={tw.style('')} />
           </TouchableOpacity>
